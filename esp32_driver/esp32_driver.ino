45b4323a36399af57e6db2f2bb2f9ea41e934d59
bjpierre <bjpierre@iastate.edu>
2021-02-28 16:07:42 -0600

    Added OTA support, note: You must have 2.7.x+ to use this

--- esp32_driver/esp32_driver.ino
+++ esp32_driver/esp32_driver.ino
@@ -4,8 +4,13 @@
  *  the most obvious difference being the different file you need to include:
  */
 #include "WiFi.h"
+#include <ESPmDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
 
-
+const char* ssid = "The515";
+const char* password ="Bluemoon696!";
+int counter;
  
 void setup()
 {
@@ -13,15 +18,51 @@ void setup()
  
     // Set WiFi to station mode and disconnect from an AP if it was previously connected
     WiFi.mode(WIFI_STA);
-    WiFi.disconnect();
+    WiFi.begin(ssid, password);
     delay(100);
+
+    bensOTABuilder();
  
     Serial.println("Setup done");
 }
- 
-void loop()
-{
-    Serial.println("scan start");
+
+void bensOTABuilder(){
+  ArduinoOTA.setHostname("CSIScanner1");
+  ArduinoOTA
+    .onStart([]() {
+      String type;
+      if (ArduinoOTA.getCommand() == U_FLASH)
+        type = "sketch";
+      else // U_SPIFFS
+        type = "filesystem";
+
+      // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+      Serial.println("Start updating " + type);
+    })
+    .onEnd([]() {
+      Serial.println("\nEnd");
+    })
+    .onProgress([](unsigned int progress, unsigned int total) {
+      Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+    })
+    .onError([](ota_error_t error) {
+      Serial.printf("Error[%u]: ", error);
+      if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+      else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+      else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+      else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+      else if (error == OTA_END_ERROR) Serial.println("End Failed");
+    });
+
+  ArduinoOTA.begin();
+
+  Serial.println("Ready");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void scanWifi(){
+  Serial.println("Scanning Starting");
  
     // WiFi.scanNetworks will return the number of networks found
     int n = WiFi.scanNetworks();
@@ -44,7 +85,14 @@ void loop()
         }
     }
     Serial.println("");
+}
  
-    // Wait a bit before scanning again
-    delay(5000);
+void loop()
+{
+    ArduinoOTA.handle();
+    if(counter++>5000){
+      counter = 0;
+      scanWifi();
+    }
+    delay(1);
 }
