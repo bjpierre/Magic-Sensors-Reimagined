978dc04433c5f8af2efb41af5204e6e3bda34f5c
rlanciloti <rlanciloti@outlook.com>
2021-04-19 00:22:27 -0500

    Updating server version number

--- Backend Server/server.py
+++ Backend Server/server.py
@@ -17,7 +17,7 @@ import tf_model_handler
 
 __author__ = "Ryan Lanciloti"
 __credits__ = ["Ryan Lanciloti"]
-__version__ = "3.1.5"
+__version__ = "3.1.10"
 __maintainer__ = "Ryan Lanciloti"
 __email__ = ["ryanjl9@iastate.edu", "rlanciloti@outlook.com"]
 __status__ = "Development"
