bd634e07df2cc5dcf233a8b8676524fe8b802707
bjpierre <bjpierre@iastate.edu>
2021-09-13 23:37:08 -0500

    Light Driver Updates, Also Added Rudimentry Arduino Code

---
+++ Arduino IDE Code/GET_POC/GET_POC.ino
@@ -0,0 +1,66 @@
+/**
+ * Authorization.ino
+ *
+ *  Created on: 09.12.2015
+ *
+ */
+
+#include <Arduino.h>
+
+#include <WiFi.h>
+#include <WiFiMulti.h>
+
+#include <HTTPClient.h>
+
+#define USE_SERIAL Serial
+
+const char* ssid = "";
+const char* password = "";
+const char* ProdServerHostName = "http://sddec21-09.ece.iastate.edu:20002/debug/get/version/server";
+WiFiMulti wifiMulti;
+
+void setup() {
+
+    USE_SERIAL.begin(115200);
+
+    USE_SERIAL.println();
+    USE_SERIAL.println();
+    USE_SERIAL.println();
+
+    for(uint8_t t = 4; t > 0; t--) {
+        USE_SERIAL.printf("[SETUP] WAIT %d...\n", t);
+        USE_SERIAL.flush();
+        delay(1000);
+    }
+
+    wifiMulti.addAP(ssid, password);
+
+}
+
+void loop() {
+    // wait for WiFi connection
+    if((wifiMulti.run() == WL_CONNECTED)) {
+
+        HTTPClient http;
+
+        USE_SERIAL.print("[HTTP] begin...\n");
+        // configure traged server and url
+
+
+        http.begin(ProdServerHostName);
+
+        USE_SERIAL.print("[HTTP] GET...\n");
+        int httpCode = http.GET();
+        if(httpCode > 0) {
+            USE_SERIAL.printf("[HTTP] GET... code: %d\n", httpCode);
+            if(httpCode == HTTP_CODE_OK) {
+                String payload = http.getString();
+                USE_SERIAL.println(payload);
+            }
+        } else {
+            USE_SERIAL.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+        }
+        http.end();
+    }
+    delay(1);
+}
