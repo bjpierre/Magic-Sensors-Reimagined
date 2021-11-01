4b84021f58572356674894bd3e71d565eb05df13
Ryan Lanciloti <rlanciloti@outlook.com>
2021-10-31 19:36:39 -0500

    updating tool

---
+++ set_builder/flask_server.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, Response
+import socket
+
+@app.route("/ml/post/inference", methods=['POST'])
+def _ml_post_inference():
+	global s
+	s.send(str(request.json).encode())
+
+
+if __name__ == "__main__":
+	data = None
+
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.connect(("localhost", 20003))
+
+	app = Flask(__name__)
+	app.run(host="0.0.0.0", port=20002)
+		
