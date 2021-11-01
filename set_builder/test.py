91a0226bc88b2699dc2dd494f5c0f96b8c83119b
Ryan Lanciloti <rlanciloti@outlook.com>
2021-10-31 21:01:44 -0500

    testing the mutex

--- set_builder/test.py
+++ set_builder/test.py
@@ -2,9 +2,12 @@ import time
 import socket
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-	s.connect(("localhost", 20002))
+	s.connect(("localhost", 20003))
 	while(True):
-		s.send(str(time.time()).encode())
-		time.sleep(0.2)
+		tmp = []
+		for i in range(255):
+			tmp.append(time.time() * i)
+		s.send(str(tmp).encode())
+		time.sleep(0.01)
 
 	
